85093d20bd755556203ad9244b062669727a3ffa
Maximgitman <gorev18091993@gmail.com>
2022-01-17 17:28:27 -0600

    Module preprocessing.py need 6 functions TODO

---
+++ preprocessing.py
@@ -0,0 +1,11 @@
+# 1. Preprocessing all_bp TODO
+
+# 2. Preprocessing time_process TODO
+
+# 3. Preprocessing skills TODO
+
+# 4. Concatenate pd.concat([all_bp, time_process, skills]) TODO
+
+# 5. Predict TODO
+
+# 6. Making Json into variable --> results TODO
