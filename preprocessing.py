06faaeba14a7f609d8f1f79f173669c15851933b
Maximgitman <gorev18091993@gmail.com>
2022-01-17 17:35:42 -0600

    Some comments for the tasks

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,11 @@
-# 1. Preprocessing all_bp TODO
+# 1. Preprocessing all_bp (maybe will it be Json) TODO
 
-# 2. Preprocessing time_process TODO
+# 2. Preprocessing time_process --> add to folder static/data TODO
 
-# 3. Preprocessing skills TODO
+# 3. Preprocessing skills --> add to folder static/data TODO
 
 # 4. Concatenate pd.concat([all_bp, time_process, skills]) TODO
 
-# 5. Predict TODO
+# 5. Predict --> model weights storage into static folder TODO
 
 # 6. Making Json into variable --> results TODO
