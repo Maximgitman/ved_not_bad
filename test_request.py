310844196981674ff1f78cca55969cf159fbf426
Maximgitman <gorev18091993@gmail.com>
2022-02-01 09:53:27 -0600

    Changed protocol for testing on localhost

--- test_request.py
+++ test_request.py
@@ -2,13 +2,12 @@ import json
 import requests
 
 
-SERVER = 'https://0.0.0.0:8080'
+SERVER = 'http://0.0.0.0:8080'
 headers = {'Content-Type': 'application/json'}
 
 with open('static/data/for_test.json') as f:
     data = json.loads(f.read())
-print(data)
-print(type(data))
+
 result = requests.post(SERVER,
                        headers=headers,
                        data=data)
